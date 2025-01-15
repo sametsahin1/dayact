3a8d4146a8aa7d389d82dfbc7d788defdde0b509
sametsahin1 <a.samed.sahinn@gmail.com>
2025-01-15 21:34:28 +0300

    docker

--- frontend/src/features/store.js
+++ frontend/src/features/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './auth/authSlice'
 import activityReducer from './activities/activitySlice'
-import logReducer from './logs/logSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     activities: activityReducer,
-    logs: logReducer,
   },
 }) 
