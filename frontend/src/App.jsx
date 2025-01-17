184d714d5e9d540e6cf478e9f91bf5a94e156e13
sametsahin1 <a.samed.sahinn@gmail.com>
2025-01-17 16:15:00 +0300

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,38 +8,43 @@ import Header from './components/Header'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Activities from './pages/Activities'
-import CompleteActivity from './pages/CompleteActivity'
 import Logs from './pages/Logs'
 import Analysis from './pages/Analysis'
-import PrivateRoute from './components/PrivateRoute'
 
 // Styles
 import './styles/main.css'
 
-function App() {
+// AppRoutes component for handling routes
+const AppRoutes = () => {
   const { user } = useSelector((state) => state.auth)
 
   return (
+    <div className="app">
+      <Header />
+      <div className="container">
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/activities" />} />
+          <Route path="/register" element={!user ? <Register /> : <Navigate to="/activities" />} />
+
+          {/* Protected Routes */}
+          <Route path="/activities" element={user ? <Activities /> : <Navigate to="/login" />} />
+          <Route path="/logs" element={user ? <Logs /> : <Navigate to="/login" />} />
+          <Route path="/analysis" element={user ? <Analysis /> : <Navigate to="/login" />} />
+
+          {/* Default Route */}
+          <Route path="*" element={<Navigate to={user ? "/activities" : "/login"} />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
     <Provider store={store}>
       <Router basename="/apps/dayact">
-        <div className="app">
-          <Header />
-          <div className="container">
-            <Routes>
-              {/* Public Routes */}
-              <Route path="/login" element={!user ? <Login /> : <Navigate to="/activities" />} />
-              <Route path="/register" element={!user ? <Register /> : <Navigate to="/activities" />} />
-
-              {/* Protected Routes */}
-              <Route path="/activities" element={user ? <Activities /> : <Navigate to="/login" />} />
-              <Route path="/logs" element={user ? <Logs /> : <Navigate to="/login" />} />
-              <Route path="/analysis" element={user ? <Analysis /> : <Navigate to="/login" />} />
-
-              {/* Default Route */}
-              <Route path="*" element={<Navigate to={user ? "/activities" : "/login"} />} />
-            </Routes>
-          </div>
-        </div>
+        <AppRoutes />
       </Router>
     </Provider>
   )
