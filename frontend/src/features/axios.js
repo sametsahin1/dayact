59af8494ecc5de990052f61c5d56f75fb1f9fd48
sametsahin1 <a.samed.sahinn@gmail.com>
2025-01-17 20:50:38 +0300

    ..

--- frontend/src/features/axios.js
+++ frontend/src/features/axios.js
@@ -1,24 +1,10 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: '/apps/dayact/api',
+  baseURL: window.location.origin + '/apps/dayact/api',
   headers: {
     'Content-Type': 'application/json'
   }
 })
-
-// Request interceptor
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // Yerel geliştirme ortamında API URL'sini değiştir
-    if (process.env.NODE_ENV === 'development') {
-      config.baseURL = 'http://localhost:5001/apps/dayact/api'
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
 
 export default axiosInstance 
